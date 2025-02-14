b9ba89302bcc634e87994c0a69e144e12b57df01
Nahaq789 <naha022677@icloud.com>
2025-02-14 14:05:25 +0900

    astronvim

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,95 +1,15 @@
--- lua/plugins/treesitter.lua
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- Customize Treesitter
+
+---@type LazySpec
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    event = { "BufReadPost", "BufNewFile" },
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter-textobjects",
-      "windwp/nvim-ts-autotag",
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "lua",
+      "vim",
+      -- add more arguments for adding more treesitter parsers
     },
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        -- 基本設定
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = false,
-        },
-
-        -- インデント設定
-        indent = { enable = true },
-
-        -- オートタグ
-        autotag = { enable = true },
-
-        -- 自動インストールする言語パーサー
-        ensure_installed = {
-          "bash",
-          "c",
-          "cpp",
-          "css",
-          "html",
-          "javascript",
-          "json",
-          "lua",
-          "markdown",
-          "markdown_inline",
-          "python",
-          "query",
-          "regex",
-          "tsx",
-          "typescript",
-          "vim",
-          "yaml",
-          "go",
-          "rust"
-        },
-
-        -- インクリメンタルセレクション
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = "<C-space>",
-            node_incremental = "<C-space>",
-            scope_incremental = false,
-            node_decremental = "<bs>",
-          },
-        },
-
-        -- テキストオブジェクト
-        textobjects = {
-          select = {
-            enable = true,
-            lookahead = true,
-            keymaps = {
-              ["af"] = "@function.outer",
-              ["if"] = "@function.inner",
-              ["ac"] = "@class.outer",
-              ["ic"] = "@class.inner",
-            },
-          },
-          move = {
-            enable = true,
-            set_jumps = true,
-            goto_next_start = {
-              ["]f"] = "@function.outer",
-              ["]c"] = "@class.outer",
-            },
-            goto_next_end = {
-              ["]F"] = "@function.outer",
-              ["]C"] = "@class.outer",
-            },
-            goto_previous_start = {
-              ["[f"] = "@function.outer",
-              ["[c"] = "@class.outer",
-            },
-            goto_previous_end = {
-              ["[F"] = "@function.outer",
-              ["[C"] = "@class.outer",
-            },
-          },
-        },
-      })
-    end,
   },
 }
