90bacdcc48b9e0620652360778e0b06ce4548bc2
Nahaq789 <naha022677@icloud.com>
2025-02-14 18:15:26 +0900

    ss

--- lua/plugins/mappings.lua
+++ lua/plugins/mappings.lua
@@ -13,10 +13,16 @@ return {
           ["<S-h>"] = { "<cmd>bprev<CR>", desc = "Go to previous buffer" },
           ["<S-l>"] = { "<cmd>bnext<CR>", desc = "Go to next buffer" },
 
+          ["ss"] = { "<cmd>split<CR>", desc = "Horizontal split" },
+          ["sv"] = { "<cmd>vsplit<CR>", desc = "Vertical split" },
+
           -- 既存の設定も維持
           ["<Leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
           ["<Leader>b"] = { desc = "Buffers" },
           ["<C-j>"] = { "<cmd>ToggleTerm<CR>", desc = "Toggle terminal" },
+        },
+        t = {
+          ["jk"] = { "<C-\\><C-n>", desc = "Switch to normal mode" },
         },
       },
     },
